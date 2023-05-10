7544f024a2abafe81fffbfae8fb1b092fda57ed8
ibouanan <130322158+ibouanan@users.noreply.github.com>
2023-05-10 22:25:53 +0200

    Add files via upload

--- SEGUNDO_PROYECTO/script.js
+++ SEGUNDO_PROYECTO/script.js
@@ -1,22 +1,24 @@
 
 
-
+//Listado de variables
 var n1,n2,vsuma,i;
 var pares,inpares
 var array = [];
 
+//Entrada de numeros
 n1 = parseInt(prompt('Ingresar numero 1'));
 n2 = parseInt(prompt('Ingresar numero 2'));
 vsuma = n1 + n2;
 
 
+//Funcion suma
 function suma(n1,n2,vsuma) {
     vsuma = n1 + n2;
     mensaje_suma="La suma de los dos numeros es ";
     return  vsuma;
 }
 
-
+//Funcion imprimir numeros pares
 function listapares(n1,n2,array) {
         
         for ( i = n1; i <= n2; i++) {
@@ -41,7 +43,7 @@ function listapares(n1,n2,array) {
     
        
 
-
+//Funcion identificar numeros primos
 function numerosprimos(n1,n2){
        if (n1 % i == 0) {
         if (n2 % i == 0) {
@@ -57,7 +59,9 @@ function numerosprimos(n1,n2){
         }
     }    
     return mensaje_primos;
-  }    
+  } 
+  
+  //Funcion para saber si los numeros son pares
   function numerospares(n1, n2) {    
     if (n1 % 2 === 0) {
         if (n2 % 2 === 0) {
@@ -74,9 +78,13 @@ function numerosprimos(n1,n2){
     }   
     return mensaje_pares;
 }
+
+//Funcion para unir todos los mensajes
 function concatenar() {
     let mensaje_concatenado = "La suma es "+ suma(n1,n2,vsuma) + "\n"  + numerospares(n1,n2) + "\n" + numerosprimos(n1,n2)  + "\n"+  "Los numeros pares intermedios son " +listapares( n1,n2,array)+ "\n";
     return mensaje_concatenado;
     }
+
+//Salida de mensaje en alert
 mensaje_alert = concatenar(); 
 alert(mensaje_alert )
